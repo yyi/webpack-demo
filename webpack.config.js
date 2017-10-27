0a799d74c4c7c3c70403808307f9beb6812f2646
yyi <yyi_mailer@gmail.com>
2017-10-27 08:46:26 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATH = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build'),
+};
+
+module.exports = {
+    entry: {
+        app: PATH.app,
+    },
+    output: {
+        path: PATH.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: 'Webpack Demo',}),
+
+    ],
+};
